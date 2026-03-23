e22a442551fad263a12c12b2dfb4330b19b50ab3
Manav <manavjoshi313@gmail.com>
2026-03-24 01:57:12 +0530

    [FEAT] Create User and Role entities

---
+++ src/main/java/com/jt/project/entity/Role.java
@@ -0,0 +1,18 @@
+package com.jt.project.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true , nullable = false)
+    private String role; // "ROLE_ADMIN" here role is prefix as SpringSecurity has in built ADMIN so to differentiate we are adding prefix as ROLE.
+}
---
+++ src/main/java/com/jt/project/entity/User.java
@@ -0,0 +1,30 @@
+package com.jt.project.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name =  "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password; //will be stored as hash
+
+    @Column(nullable = false , unique = true)
+    private String email;
+
+    // Many users can have one role
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+}
