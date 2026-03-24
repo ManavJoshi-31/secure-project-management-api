015e837e506f20e6d7e32fb1902a6afb2208ec1e
Manav <manavjoshi313@gmail.com>
2026-03-24 23:24:08 +0530

    [SECURITY] add PasswordEncoder bean in AppConfig

--- src/main/java/com/jt/project/security/AppConfig.java
+++ src/main/java/com/jt/project/security/AppConfig.java
@@ -1,4 +1,13 @@
 package com.jt.project.security;
+import org.springframework.context.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
