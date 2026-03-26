57a56d40028c3d89d524032b249dfb364b4c516e
Manav <manavjoshi313@gmail.com>
2026-03-27 01:37:19 +0530

    [FEAT] add Two Custom Exceptions ResourceNotFound and BuisnessRule Exceptions

---
+++ src/main/java/com/jt/project/exception/BuisnessRuleException.java
@@ -0,0 +1,7 @@
+package com.jt.project.exception;
+
+public class BuisnessRuleException extends RuntimeException{
+    public BuisnessRuleException(String message){
+        super(message);
+    }
+}
