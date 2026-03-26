0a3b90547c100ca47021c477323b6ce3d89c4275
Manav <manavjoshi313@gmail.com>
2026-03-26 17:05:19 +0530

    [REFACTOR] add sample manual implementation of repository menthod

--- src/main/java/com/jt/project/repository/ProjectRepository.java
+++ src/main/java/com/jt/project/repository/ProjectRepository.java
@@ -7,7 +7,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    /* Manual Implementation using JPQL
+    @Query("SELECT p FROM Project p WHERE p.createdBy = :createdBy")
+    List<Project> getProjectsByUser(@Param("createdBy") User createdBy);
+     */
 
+    /* Manual Implementation using EntityManager
+        @PersistenceContext
+        private EntityManager entityManager;
+        public List<Project> findProjectsByUser(User user) {
+        String jpql = "SELECT p FROM Project p WHERE p.createdBy = :user";
+        return entityManager.createQuery(jpql, Project.class)
+                .setParameter("user", user)
+                .getResultList();
+        }
+     */
     List<Project> findByCreatedBy(User createdBy);
 
     List<Project> findByStatus(ProjectStatus status);
