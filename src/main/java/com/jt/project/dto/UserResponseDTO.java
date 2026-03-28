a73f9f8a52e25be53a277c5487eb7b2c6d177d2c
Manav <manavjoshi313@gmail.com>
2026-03-28 23:40:08 +0530

    [FIX] dataType changed from long to int in UserResponseDTO

--- src/main/java/com/jt/project/dto/UserResponseDTO.java
+++ src/main/java/com/jt/project/dto/UserResponseDTO.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserResponseDTO {
-    private Long id;
+    private int id;
     private String username;
     private String email;
     private String role;
