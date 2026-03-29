491adde5aa9f9b7f5f48f242eb6d11cbc7cd96bc
Manav <manavjoshi313@gmail.com>
2026-03-29 17:21:52 +0530

    [FEAT] add AuthController to handle register request

--- src/main/java/com/jt/project/controller/AuthController.java
+++ src/main/java/com/jt/project/controller/AuthController.java
@@ -1,4 +1,43 @@
 package com.jt.project.controller;
 
+import com.jt.project.dto.RegisterRequestDTO;
+import com.jt.project.dto.UserResponseDTO;
+import com.jt.project.entity.User;
+import com.jt.project.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    private UserResponseDTO mapToDTO(User user) {
+        UserResponseDTO dto = new UserResponseDTO();
+        dto.setId(user.getId());
+        dto.setUsername(user.getUsername());
+        dto.setEmail(user.getEmail());
+        dto.setRole(user.getRole().getRole());
+        return dto;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> register( @RequestBody RegisterRequestDTO request) {
+        User user = userService.registerUser(
+                request.getUsername(),
+                request.getPassword(),
+                request.getEmail(),
+                request.getRoleName()
+        );
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(mapToDTO(user));
+    }
+
 }
